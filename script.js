6edcfd99a75652e8bb22f3270728b758a059e1f2
Tynahan <nusilite@gmail.com>
2025-06-03 20:34:37 +0900

    update lottary

---
+++ script.js
@@ -0,0 +1,21 @@
+function PageInfo(day, title, href) {
+				this.day = day;
+				this.title = title;
+				this.href = href;
+}
+
+const pageInfoDict = [
+				new PageInfo(4, "1. 프로필 페이지", "./4일차/1_프로필_페이지/"),
+				new PageInfo(4, "2. 채팅방", "./4일차/1_프로필_페이지/"),
+				new PageInfo(4, "3. 눈사람", "./4일차/1_프로필_페이지/"),
+				new PageInfo(4, "4. 레이아웃", "./4일차/1_프로필_페이지/"),
+				new PageInfo(5, "1. 채팅방 말풍선", "./4일차/1_프로필_페이지/"),
+				new PageInfo(5, "2.", "./"),
+				new PageInfo(5, "3.", "./"),
+				new PageInfo(5, "4.", "./"),
+				new PageInfo(5, "5.", "./"),
+				new PageInfo(5, "6.", "./"),
+				new PageInfo(8, "1.", "./"),
+				new PageInfo(8, "2.", "./"),
+				new PageInfo(8, "3.", "./"),
+]
